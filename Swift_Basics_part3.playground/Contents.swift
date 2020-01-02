120a6db566c1bc1ddbea7dff3c153db4a73bbecd
Virendra Gupta <vgupta@ivycomptech.com>
2020-01-02 11:47:44 +0530

    first commit

---
+++ Swift_Basics_part3.playground/Contents.swift
@@ -0,0 +1,33 @@
+
+/* This playground contains explanation and code for the below topics in Swift 5.1
+1.
+*/
+
+
+import UIKit
+
+//Basic Operators
+
+/*
+ Terminology
+ 
+ Operators are unary, binary, Or ternary:
+ -> Unary operators operate on a sigle target (such as -a)
+ 
+ -> Binary operators operate on two targets (Such 2 + 3)
+ 
+ -> ternary condition (a ? b : c).
+ 
+ The value that Operators affect are operands. In the expression 1 + 2 , the + symbol is a binary opeator
+ and its two operands are the value 1 and 2.
+ 
+ */
+
+
+//Assignment Operator
+
+/*
+ The assig
+ */
+
+
