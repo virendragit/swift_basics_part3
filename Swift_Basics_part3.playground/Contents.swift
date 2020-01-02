7a6aca5032a05ade2c7fa2d31afac887aee8754f
Virendra Gupta <vgupta@ivycomptech.com>
2020-01-02 12:08:26 +0530

    added unary binry and ternary and reminder Operator

--- Swift_Basics_part3.playground/Contents.swift
+++ Swift_Basics_part3.playground/Contents.swift
@@ -27,7 +27,35 @@ import UIKit
 //Assignment Operator
 
 /*
- The assig
+ The assignment operator (a = b) initializes or updates the value of a with the value of b :
  */
+
+let b = 4
+
+var a = -9
+
+let reminder = (a % b)
+
+a =  b // a is now equal to 10
+
+
+//let (x, y) = (1, 2)
+
+// Unlike the assginment operator in C and Objective-C, The assigment opertor in swift does not itself retun a value.
+
+
+//Below is not a valid stmt, Because x = y does not return a value.
+/*
+if x = y {
+    
+}
+ */
+
+//"hello, " + "world"
+
+//Remainder Operator
+
+let reminder = (a % b)
+
 
 
